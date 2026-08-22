d31ea861869e2ba1ca87afb0c8dbc4d168139620
Tran Van Thai <tranvanthai20062@gmail.com>
2026-08-22 13:14:46 +0700

    [FIX] Sync calendar view task grouping using unified startOfDay timestamp calculation

--- Code/TaskManagementApp/app/src/main/java/com/team/taskmanagementapp/ui/CalendarFragment.kt
+++ Code/TaskManagementApp/app/src/main/java/com/team/taskmanagementapp/ui/CalendarFragment.kt
@@ -26,6 +26,7 @@ import com.team.taskmanagementapp.data.repository.TaskRepository
 import com.team.taskmanagementapp.databinding.FragmentCalendarBinding
 import com.team.taskmanagementapp.ui.detail.TaskDetailActivity
 import com.team.taskmanagementapp.util.Constants
+import com.team.taskmanagementapp.util.DateTimeUtils
 import com.team.taskmanagementapp.viewmodel.CalendarViewModel
 import com.team.taskmanagementapp.viewmodel.CalendarViewModelFactory
 import kotlinx.coroutines.flow.combine
@@ -193,12 +194,8 @@ class CalendarFragment : Fragment() {
 
         val dayCal = (tempCal.clone() as Calendar).apply {
             set(Calendar.DAY_OF_MONTH, day)
-            set(Calendar.HOUR_OF_DAY, 0)
-            set(Calendar.MINUTE, 0)
-            set(Calendar.SECOND, 0)
-            set(Calendar.MILLISECOND, 0)
         }
-        val dayKey = dayCal.timeInMillis
+        val dayKey = DateTimeUtils.getStartOfDay(dayCal.timeInMillis)
 
         val dayText = TextView(requireContext()).apply {
             text = day.toString()
